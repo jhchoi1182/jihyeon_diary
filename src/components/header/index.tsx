20a997d6b0b33760e43af4459a8c481f71b31766
jhchoi1182 <jhchoi1182@gmail.com>
2026-02-06 18:11:00 +0900

    fix: 헤더 컴포넌트의 반응형 레이아웃 개선

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -5,11 +5,11 @@ import { AiFillHome } from "react-icons/ai";
 
 export default function Header() {
   return (
-    <header className="sticky top-0 z-10 flex justify-between items-center p-4 bg-river dark:bg-lightDark">
+    <header className="sticky top-0 z-10 flex flex-wrap justify-between items-center p-4 bg-river dark:bg-lightDark sm:flex-col sm:items-start">
       <Link href="/">
-        <h1 className="text-3xl font-bold slate-900 pl-10 sm:w-10">{"jihyeon's Blog"}</h1>
+        <h1 className="text-3xl font-bold slate-900 pl-10 whitespace-nowrap sm:pl-0">{"jihyeon's Blog"}</h1>
       </Link>
-      <nav className="flex sm:block items-center text-white pr-10">
+      <nav className="flex sm:block sm:w-full items-center text-white pr-10 sm:pr-0 sm:pt-2">
         <div className="mr-4 sm:flex sm:justify-end sm:mb-4 sm:mr-0">
           <Search />
         </div>
